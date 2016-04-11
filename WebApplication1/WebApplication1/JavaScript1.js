b268425659b22add0069f2d61bb020d7992abf3f
valentinaAdz <valentina.adz@gmail.com>
2016-04-11 15:54:56 +0200

    Added files via upload

---
+++ WebApplication1/WebApplication1/JavaScript1.js
@@ -0,0 +1,24 @@
+﻿
+function checkCities() {
+
+    var cities = ["Берово", "Скопје", "Куманово", "Охрид", "Тетово"];
+    var input = document.getElementById("city").value;
+    var isInMK = false;
+
+
+    for (i = 0; i < cities.length; i++) {
+        if (input == cities[i]) {
+            isInMK = true;
+        }
+    }
+
+    if (isInMK) {
+        alert("Градот е во Македонија");
+        document.getElementById("show").innerHTML = (input);
+
+    } else {
+        alert("Градот не е во Македонија");
+
+    }
+
+}
